5dc25304fa9afd792c7e8d591a6538a1d120f9f5
Fadlian17 <alfansyahfadlian@gmail.com>
2020-06-17 10:39:31 +0700

    hari ke 3

---
+++ h3/python-cli/tes.py
@@ -0,0 +1,20 @@
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(name='gen')
+def generic():
+    click.echo('Hello there')
+
+
+@cli.command(name='wel')
+def welcome():
+    click.echo('Welcome')
+
+
+if __name__ == '__main__':
+    cli()
